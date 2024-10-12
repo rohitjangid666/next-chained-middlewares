b001bcd1a5db25e55409e6b1624d3bfdcb24316d
Rohit Jangid <73385164+rohitjangid666@users.noreply.github.com>
2024-10-12 11:53:48 +0530

    feat: tailwind configuration

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div
-      style={{
-        height: 50,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        backgroundColor: '#fff',
-        position: 'fixed',
-        color: '#000',
-        top: 0,
-        left: 0,
-        gap: '1rem',
-      }}
-    >
+    <div className='h-[50px] flex justify-center items-center w-full bg-rose-600 fixed top-0 left-0 text-white font-semibold gap-4'>
       <Link href='/'>HOME</Link>
       <Link href='/about'>ABOUT</Link>
     </div>
